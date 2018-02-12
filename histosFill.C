d59c31698d6f4c2548727f6f637009911cbbf2db
Hannu Siikonen <hannu.siikonen@gmail.com>
2018-02-12 10:29:40 +0200

    Hotfix to a simple cout bug

--- histosFill.C
+++ histosFill.C
@@ -665,7 +665,7 @@ void histosFill::Loop()
 
         // check for non-zero PU weight
         _pass = _pass and wtrue!=0;
-        if (wtrue==0) cout "Zero PU weight!" << endl;
+        if (wtrue==0) cout << "Zero PU weight!" << endl;
       }
     } // for itrg
     _wt["mc"] = _wt[_jp_reftrig];
